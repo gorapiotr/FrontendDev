1a31cd8ad92985374b87c3d3060fe82f8492e463
wdunia <32812715+wdunia@users.noreply.github.com>
2018-03-26 20:26:58 +0200

    Naprawiona transpilacja

--- lab4/lab4-compiled.js
+++ lab4/lab4-compiled.js
@@ -14,10 +14,10 @@
 
     Nowości w ES6
         - lambda
-        - Clesses
+        - Clesses + dziedziczenie
         - template strings
         - default + rest + spred
-        - let
+        - let, const (!!!)
         - iterator + for... of 
 */
 
@@ -31,17 +31,23 @@ doubles.array.forEach(item => console.log(item));
 
 // Przykład 2
 
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
 console.log("\nPrzykład 2");
-var evenPredicate = v => v % 2 === 0;
+var evenPredicate = function evenPredicate(v) {
+    return v % 2 === 0;
+};
 var evens = [1, 3, 4, 5, 6].filter(evenPredicate);
-evens.forEach(v => console.log(v));
+evens.forEach(function (v) {
+    return console.log(v);
+});
 
 // Przykład 3
 console.log("\nPrzykład 3");
 var aShape = {
     numbers: [1, 2, 3, 4, 5],
     scale: 2,
-    move() {
+    move: function move() {
         var self = this; // zapamiętanie this aby używać wewnątrz poniższej funkcji
         this.numbers = this.numbers.map(function (element) {
             return element * self.scale;
@@ -49,7 +55,7 @@ var aShape = {
     }
 };
 aShape.move();
-aShape.numbers.forEach(element => {
+aShape.numbers.forEach(function (element) {
     console.log(element);
 });
 
@@ -58,21 +64,49 @@ console.log("\nPrzykład 3 - lambda");
 var aShape = {
     numbers: [1, 2, 3, 4, 5],
     scale: 2,
-    move() {
+    move: function move() {
+        var _this = this;
+
         // var self = this; // zapamiętanie this aby używać wewnątrz poniższej funkcji
-        this.numbers = this.numbers.map(element => element * this.scale);
+        this.numbers = this.numbers.map(function (element) {
+            return element * _this.scale;
+        });
     }
 };
 aShape.move();
-aShape.numbers.forEach(element => {
+aShape.numbers.forEach(function (element) {
     console.log(element);
 });
 
 // Przykład 4 klasy
 console.log("\nPrzykład 4 - klasy");
-class Shape {
-    constructor(x, y) {
+
+var Shape = function () {
+    function Shape(x, y) {
+        _classCallCheck(this, Shape);
+
         this.x = x;
         this.y = y;
     }
-}
+
+    Shape.prototype.move = function move(delta) {
+        this.x += delta;
+        this.y += delta;
+    };
+
+    Shape.prototype.moveToZero = function moveToZero() {
+        this.x = 0;
+        this.y = 0;
+    };
+
+    return Shape;
+}();
+
+var aShape = new Shape(2, 3);
+aShape.move(5);
+console.log(aShape.x);
+
+/*
+    Zadanie: połączyć zadanie 1 i 2 i zapisać w ES6 z użyciem nowych funkcji
+    https://babeljs.io/learn-es2015/
+*/
