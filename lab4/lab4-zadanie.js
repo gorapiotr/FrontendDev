3c539cdd55fc72ade71e3ccc9156d02d491fc3fb
wdunia <32812715+wdunia@users.noreply.github.com>
2018-04-03 19:01:18 +0200

    Zadanie z Lab4 - rozbudowa
    ES6 - klasy, dziedziczenie, użycie nowych funkcji

--- lab4/lab4-zadanie.js
+++ lab4/lab4-zadanie.js
@@ -1,4 +1,6 @@
-
+/*
+    Klasa obsługująca operacje na tabeli narzędzi i typów
+*/
 class Tools {
     
     constructor() {
@@ -36,76 +38,121 @@ class Tools {
 
         // Wyświetlanie listy i typów wszystkich narzędzi
         getToolsTypes() {
-            console.log("\nLista narzędzi:");
-            this.narzedzia.forEach(function(element, index) {
-                //console.log("\t%s (kategoria: %s)",element.nazwa, this.searchType(element.typ));
-            });
+            let output;
+            this.output = '\nLista narzędzi:\n';
+            this.narzedzia.forEach(element => 
+                this.output += ("\t"+element.nazwa+" (kategoria: "+this.searchType(element.typ)+")\n")
+            );
+            return this.output;
         }
 
         // Wyświetlanie typu pojedynczego narzędzia
-        getToolType(toolName) {
-            console.log("\nZnaleziono narzędzie:");
+        // jeśli printDescription===false to nie wyświetlamy opisów
+        getToolType(toolName, printDescription = true) {
             this.findTool = this.narzedzia.find(element => element.nazwa===toolName);
-            console.log("\t" + this.findTool.nazwa + " (kategoria: "+ this.searchType(this.findTool.typ) +")");
+            if (printDescription) { 
+                let output="";              
+                this.output = '\nZnaleziono narzędzie:\n'; 
+                this.output += ("\t" + this.findTool.nazwa + " (kategoria: "+ this.searchType(this.findTool.typ) +")");
+            } else 
+            {              
+                this.output = this.findTool.typ;
+            }
+            return this.output;
         }
 
         // Wyświetlenie stanu magazynowego narzędzia
         getToolStock(toolName) {
-            console.log("\nStan magazynowy:");
+            let output;
+            this.output = '\nStan magazynowy:\n';
             this.findTool = this.narzedzia.find(element => element.nazwa===toolName);
-            console.log("\t" + this.findTool.nazwa + ": "+ this.findTool.stan + " szt.");
+            this.output += ("\t" + this.findTool.nazwa + ": "+ this.findTool.stan + " szt.");
+            return this.output;
         }
 
         // Kupno narzędzi
         buyTool(toolName) {
-            console.log("\nZakupiono narzędzie:");
+            let output;
+            this.output = '\nZakupiono narzędzie:\n';
             this.findTool = this.narzedzia.find(element => element.nazwa===toolName);
-            console.log("\t"+ this.findTool.nazwa);
+            this.output += ("\t"+ this.findTool.nazwa);
             this.findTool.stan++;
+            return this.output;
         }
 
         // Sprzedaż narzędzi
         sellTool(toolName) {
-            console.log("\nSprzedano narzędzie:");
+            let output;
+            this.output = '\nSprzedano narzędzie:\n';
             this.findTool = this.narzedzia.find(element => element.nazwa===toolName);
-            console.log("\t"+ this.findTool.nazwa);
+            this.output += ("\t"+ this.findTool.nazwa);
             if (this.findTool.stan>0) {
                 this.findTool.stan--;
             }
+            return this.output;
         }
 
         // Dodanie narzędzia do listy
         addTool(toolName,toolType) {
             let newTool = {};
+            let output;
             this.findTool = this.narzedzia.find(element => element.nazwa===toolName);
             if (!this.findTool) {
                 this.findType = this.typy[toolType];
                 if (this.findType) {
                     newTool.nazwa = toolName;
-                    newTool.typ = typy[toolType];
+                    newTool.typ = this.typy[toolType];
                     newTool.stan = 0;
-                    narzedzia.push(newTool);
-                    console.log("\nDodano narzędzie do listy.");
+                    this.narzedzia.push(newTool);
+                    this.output = ("\nDodano narzędzie: "+toolName+" do listy.");
                 } else {
-                    console.log("\nPodano niewłaściwy typ narzędzia.");
+                    this.output = ("\nPodano niewłaściwy typ narzędzia: "+ toolType +".");
                 }
             } else {
-                console.log("\nNarzędzie istnieje już w bazie.");
+                this.output = ("\nNarzędzie: "+toolName+", istnieje już w bazie.");
             }
+            return this.output;
         }
 }
 
-var toolBox = new Tools;
+/* 
+    Klasa rozbudowująca funkcjonalność o funkcje do obsługi narzędzi elektrycznych
+    dziedzicząca po klasie Tools
+*/
+class ElectricTools extends Tools {
+    
+    // Rozszerzamy o fukcję wyświetlania na ekranie
+    print(input) {
+        console.log(input);
+    }
 
-toolBox.getToolsTypes();
+    // Rozszerzamy funkcjonalność dla narzędzi elektrycznych
+    turnPowerOn(toolName) {
+        let output="";
+        if (this.getToolType(toolName,false)==this.typy.ELEKTRYCZNE) {
+            this.output = '\nPodłączamy narzędzie: '+ toolName +', do prądu:\n';          
+        } else
+        {
+            this.output = '\nNarzędzia: '+ toolName +', nie można podłączyć do prądu.\n';                     
+        }
+        return this.output;
+    }
+} 
 
-toolBox.getToolType("Miara");
-toolBox.buyTool("Miara");
-toolBox.buyTool("Miara");
-toolBox.getToolStock("Miara");
-toolBox.sellTool("Miara");
-toolBox.getToolStock("Miara");
-toolBox.addTool("Ekierka","POMIAROWE");
-toolBox.getToolsTypes();
-toolBox.addTool("Ekierka","POMIAROWE");
-toolBox.getToolsTypes();
+// tworzenie obiektu klasy ElectricTools i testowanie
+var toolBox = new ElectricTools;
+toolBox.print(toolBox.getToolsTypes());
+toolBox.print(toolBox.getToolType("Miara"));
+toolBox.print(toolBox.buyTool("Miara"));
+toolBox.print(toolBox.buyTool("Miara"));
+toolBox.print(toolBox.getToolStock("Miara"));
+toolBox.print(toolBox.sellTool("Miara"));
+toolBox.print(toolBox.getToolStock("Miara"));
+toolBox.print(toolBox.addTool("Ekierka","POMIAROWE"));
+toolBox.print(toolBox.getToolsTypes());
+toolBox.print(toolBox.addTool("Ekierka","POMIAROWE"));
+toolBox.print(toolBox.getToolsTypes());
+
+// testowanie rozszerzonej klasy
+toolBox.print(toolBox.turnPowerOn("Młotek"));
+toolBox.print(toolBox.turnPowerOn("Szlifierka"));
