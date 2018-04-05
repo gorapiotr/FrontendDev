6f45cabd50e41be90dd2f789ece68af82d8b70e3
wdunia <32812715+wdunia@users.noreply.github.com>
2018-04-05 18:35:12 +0200

    Zadanie z Lab3 - rozbudowa
    Dodano typy i nazwy narzędzi. Dodano opisy.

--- lab3/lab3-2.js
+++ lab3/lab3-2.js
@@ -1,53 +1,109 @@
 /*  
     =========================================
-    Dziedziczenie po klasie narzędzia
+    Klasa Tools
+    =========================================
 */
-function Narzedzia (nazwa) {
+// Constructor
+function Tools (nazwa) {   
+    this.typy = {
+        PROSTE: 0,
+        ELEKTRYCZNE: 1,
+        PNEUMATYCZNE: 2,
+        POMIAROWE: 3,
+        POMOCNICZE: 4
+    };  
+    
+    this.narzedzia = [
+        { nazwa: "Młotek", typ: [this.typy.PROSTE], stan: 1},
+        { nazwa: "Wiertarka", typ: [this.typy.ELEKTRYCZNE], stan: 1},
+        { nazwa: "Młot", typ: [this.typy.PNEUMATYCZNE], stan: 1},
+        { nazwa: "Spawarka", typ: [this.typy.ELEKTRYCZNE], stan: 1},
+        { nazwa: "Szlifierka", typ: [this.typy.ELEKTRYCZNE], stan: 1},
+        { nazwa: "Kombinerki", typ: [this.typy.PROSTE], stan: 1},
+        { nazwa: "Miara", typ: [this.typy.POMIAROWE], stan: 1},
+        { nazwa: "Pion", typ: [this.typy.POMIAROWE], stan: 1},
+        { nazwa: "Wkrętak", typ: [this.typy.PROSTE], stan: 1},
+        { nazwa: "Imadło", typ: [this.typy.POMOCNICZE], stan: 1}
+        ];
+    
     this.nazwa = nazwa;
 }
 
-Narzedzia.prototype = {
-    uzyj: function () {
-        return "Po prostu używam narzędzie: "+ this.nazwa;
-    }
+// Wyszukanie typu po indeksie
+Tools.prototype.searchType = function (typeNo) {
+        this.wynik = "";
+        for (typ in this.typy) {
+            if (this.typy[typ] == typeNo) {
+                this.wynik = typ;
+            }
+
+        }
+        return this.wynik;
 }
 
-Narzedzia.prototype.getNazwa = function () {
+Tools.prototype.uzyj = function (toolName) {
+        findTool = this.narzedzia.find(function(element){
+            return element.nazwa===toolName;
+        }); 
+        return "Używam narzędzie: "+toolName + ", typu: " + Tools.prototype.searchType.call(this,findTool.typ);
+}
+
+
+Tools.prototype.getNazwa = function () {
     return this.nazwa;
 }
 
-// Elektryczne dziedziczy ========================================
+/*  
+    =========================================
+    Klasa Elektryczne dziedziczy po Tools
+    =========================================
+*/
+// Constructor
 function Elektryczne (nazwa,dodatkowanazwa) {
-    Narzedzia.call(this, nazwa); // wywołanie
+    Tools.call(this, nazwa); // wywołanie konstruktora Tools
     this.dodatkowanazwa = dodatkowanazwa;
 }
 
-Elektryczne.prototype = Object.create(Narzedzia.prototype);
+Elektryczne.prototype = Object.create(Tools.prototype);
 Elektryczne.prototype.constructor = Elektryczne;
 
-// Rozszerzanie
+// Rozszerzanie klasy elektryczne o nowa metode
 Elektryczne.prototype.jakUzywacElektryczne = function() {
     return "Najpierw musisz podłączyć do prądu.";
 }
 
-// Hudrauliczne dziedziczy ========================================
+/*  
+    =========================================
+    Klasa Hydrauliczne dziedziczy po Tools
+    =========================================
+*/
+// Constructor
 function Hydrauliczne (nazwa,dodatkowanazwa) {
-    Narzedzia.call(this, nazwa); // wywołanie
+    Tools.call(this, nazwa); // wywołanie konstruktora Tools
     this.dodatkowanazwa = dodatkowanazwa;
 }
 
-Hydrauliczne.prototype = Object.create(Narzedzia.prototype);
+Hydrauliczne.prototype = Object.create(Tools.prototype);
 Hydrauliczne.prototype.constructor = Hydrauliczne;
 
-// Rozszerzanie
+// Rozszerzanie klasy hydrauliczne o nawa metode
 Hydrauliczne.prototype.jakUzywacHydrauliczne = function() {
     return "Najpierw musisz podłączyć do sprężarki.";
 }
 
+/*  
+    =========================================
+    Tworzymy obiekty i testujemy
+    =========================================
+*/
+var NewElektryczne = new Elektryczne ("Wiertarka","220V");
+console.log(NewElektryczne.uzyj("Wiertarka"));
+console.log(NewElektryczne.jakUzywacElektryczne());
+console.log(NewElektryczne.nazwa);
+console.log(NewElektryczne.dodatkowanazwa);
 
-// Tworzymy obiekt
-var nElektryczne = new Elektryczne ("Wiertarka","220V");
-console.log(nElektryczne.uzyj());
-console.log(nElektryczne.jakUzywacElektryczne());
-console.log(nElektryczne.nazwa);
-console.log(nElektryczne.dodatkowanazwa);
+var NewHydrauliczne = new Hydrauliczne ("Młot","10bar");
+console.log(NewHydrauliczne.uzyj("Młot"));
+console.log(NewHydrauliczne.jakUzywacHydrauliczne());
+console.log(NewHydrauliczne.nazwa);
+console.log(NewHydrauliczne.dodatkowanazwa);
