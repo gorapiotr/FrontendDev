ec079bb3973285b09d54fd8f63237cebcd35ee96
wdunia <32812715+wdunia@users.noreply.github.com>
2018-04-11 15:29:16 +0200

    Lab5 - zadanie 1

--- lab5/app.js
+++ lab5/app.js
@@ -1,17 +1,26 @@
 var q = $('#q');
 var  keyups = Rx.Observable.fromEvent(q, 'keyup');
 
-keyups.throttleTime(500).map(function(x) {
+keyups.throttleTime(500)
+    .map(function(x) {
         return q.val();
     })
     .switchMap(function(x) {
-        return searchWikipedia(x);
+        return searchWikipedia(x);                    
     })
     .do(function(x) {
         console.log(x);
+        $('#results').html(""); // Kasowanie poprzedniego wyniku
     })
     .subscribe(function(x) {
-        $('#results').text(x);
+        x[1].forEach((element,i) => {
+            $('#results').append("<div>" 
+                + element
+                +" <a href='" +x[3][i]+ "'>" +x[3][i]
+                + "</a></div>"
+                //+ "<div>"+x[2][i]+"</div>"
+            );          
+        });
     });
 
 function searchWikipedia(term) {
