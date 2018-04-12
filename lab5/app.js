de5b5432090092e7e922916d9b161292294e5461
wdunia <32812715+wdunia@users.noreply.github.com>
2018-04-12 09:36:32 +0200

    Lab5 - zadanie 1 - try catch, string template

--- lab5/app.js
+++ lab5/app.js
@@ -1,3 +1,7 @@
+/*
+    Wyszukiwarka Wikipedia
+*/
+
 var q = $('#q');
 var  keyups = Rx.Observable.fromEvent(q, 'keyup');
 
@@ -6,21 +10,26 @@ keyups.throttleTime(500)
         return q.val();
     })
     .switchMap(function(x) {
-        return searchWikipedia(x);                    
+            return searchWikipedia(x);                                
     })
     .do(function(x) {
-        console.log(x);
+        //console.log(x);
         $('#results').html(""); // Kasowanie poprzedniego wyniku
     })
     .subscribe(function(x) {
-        x[1].forEach((element,i) => {
-            $('#results').append("<div>" 
-                + element
-                +" <a href='" +x[3][i]+ "'>" +x[3][i]
-                + "</a></div>"
-                //+ "<div>"+x[2][i]+"</div>"
-            );          
-        });
+        try {
+            x[1].forEach((element,i) => {
+                $('#results').append(
+                    `<div class='search-block'> 
+                    <b>${element}</b>
+                    <a href='${x[3][i]}'>${x[3][i]}</a>
+                    <div class='description'>${x[2][i]}</div>
+                    </div>`
+                );          
+            });   
+        } catch(err) {
+            //console.log('Brak wyników');
+        }
     });
 
 function searchWikipedia(term) {
